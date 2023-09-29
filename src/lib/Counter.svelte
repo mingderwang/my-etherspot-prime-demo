21c3af336307bbb89da9d669ea343178765b7434
Ming-der Wang <mosed168@gmail.com>
2023-09-29 10:41:31 +0800

    Initial commit

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,15 @@
+<script>
+    import {Button, Heading} from "@kahi-ui/framework";
+
+    let clicks = 0;
+
+    function on_click(event) {
+        clicks++;
+    }
+</script>
+
+<Heading>
+    Clicks: {clicks}
+</Heading>
+
+<Button on:click={on_click}>Add +1</Button>

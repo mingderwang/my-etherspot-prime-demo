49d57a8f78c6208dca8d7b170ad1bf82cfa8aafd
Ming-der Wang <mingderwang@gmail.com>
2023-10-10 13:38:48 +0800

    tmp

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,10 +1,11 @@
 <script>
     import {Button, Heading} from "@kahi-ui/framework";
-
+    //import { PrimeSdk } from '@etherspot/prime-sdk';
     let clicks = 0;
-
     function on_click(event) {
         clicks++;
+   //     const primeSdk = new PrimeSdk({ privateKey: process.env.VITE_WALLET_PRIVATE_KEY }, { chainId: 80001, projectKey: '' })
+  //  primeSdk.getCounterFactualAddress().then(console.log)
     }
 </script>
 
